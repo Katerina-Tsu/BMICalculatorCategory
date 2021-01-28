8cecb1bd16d33b3a883ed7a1d68b87e8f68f5b08
Katerina-Tsu <k.tsupikova@outlook.com>
2021-01-28 15:39:25 +0300

    Created test-method calculateStarvation in CategoryTest

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,2 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class CategoryTest {
+
+    @Test
+    public void calculateStarvation () {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://healthunify.com/bmicalculator/");
+        driver.findElement(By.name("wg")).sendKeys("40");
+        driver.findElement(By.name("ht")).sendKeys("174");
+        driver.findElement(By.name("cc")).click();
+        String actualCategoryText = driver.findElement(By.name("desc")).getAttribute("value");
+        String expectedCategoryText = "Your category is Starvation";
+        Assert.assertEquals(actualCategoryText, expectedCategoryText, "Incorrect category is displayed: " + actualCategoryText);
+        driver.close();
+
+    }
+
 }
