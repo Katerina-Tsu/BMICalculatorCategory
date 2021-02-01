0e7120e71eedd4fcdeb0785ede15e8246374ce1f
Katerina-Tsu <k.tsupikova@outlook.com>
2021-02-01 14:34:22 +0300

    Renamed class StarvationCategoryTest.java

--- src/test/java/StarvationCategoryTest.java
+++ src/test/java/StarvationCategoryTest.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CategoryTest {
+public class StarvationCategoryTest {
 
     @Test
     public void calculateStarvation () {
